4dd93cf440507e6fa68d7a6b741bc60b141ecbc4
ANDREAS PAPADIAS <andreaspapadias@gmail.com>
2022-07-13 20:57:18 +0300

    admin dashboard is ready

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,35 +25,3 @@
     </script>
 </body>
 </html>
-=======
-<?php>
-    include_once 'header.php';
-<?>
-
-    <section class="index-intro">
-        <h1>Welcome to Admin's Main Page</h1>
-    </section>
-
-<<<<<<< HEAD
-    <section class="index-categories">
-    
->>>>>>> 126ccdc (admin dashboard is ready)
-=======
-    <?php
-        if(isset($_POST['btn-u'])){
-            echo 'You just clicked Upload button';
-        }
-        if(isset($_POST['btn-e'])){
-            echo 'You just clicked Edit button';
-        }
-        if(isset($_POST['btn-dd'])){
-            echo 'You just clicked Delete Data button';
-        }
-    ?>
-    <form method="post"> 
-        <input type="submit" name="btn-u" value ="Upload"> 
-        <input type="submit" name="btn-e" value ="Edit"> 
-        <input type="submit" name="btn-dd" value ="Delete Data"> 
-    </form>
-      
->>>>>>> eff02e3 (insert/edit/delete buttons)
