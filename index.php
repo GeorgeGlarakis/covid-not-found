126ccdc2fc08f9926a5ecf50327dfb961602a070
ANDREAS PAPADIAS <andreaspapadias@gmail.com>
2022-07-13 20:57:18 +0300

    admin dashboard is ready

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php>
+    include_once 'header.php';
+<?>
+
+    <section class="index-intro">
+        <h1>Welcome to Admin's Main Page</h1>
+    </section>
+
+    <section class="index-categories">
+    
