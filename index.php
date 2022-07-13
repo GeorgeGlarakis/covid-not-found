949613d45a45477638ef073ce1d47acd86662a0a
ANDREAS PAPADIAS <andreaspapadias@gmail.com>
2022-07-13 22:51:55 +0300

    insert/edit/delete buttons

--- index.php
+++ index.php
@@ -35,6 +35,26 @@
         <h1>Welcome to Admin's Main Page</h1>
     </section>
 
+<<<<<<< HEAD
     <section class="index-categories">
     
 >>>>>>> 126ccdc (admin dashboard is ready)
+=======
+    <?php
+        if(isset($_POST['btn-u'])){
+            echo 'You just clicked Upload button';
+        }
+        if(isset($_POST['btn-e'])){
+            echo 'You just clicked Edit button';
+        }
+        if(isset($_POST['btn-dd'])){
+            echo 'You just clicked Delete Data button';
+        }
+    ?>
+    <form method="post"> 
+        <input type="submit" name="btn-u" value ="Upload"> 
+        <input type="submit" name="btn-e" value ="Edit"> 
+        <input type="submit" name="btn-dd" value ="Delete Data"> 
+    </form>
+      
+>>>>>>> eff02e3 (insert/edit/delete buttons)
