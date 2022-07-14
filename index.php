7c0d73a4f4dd584c3c1e883501728e125d3cf888
ANDREAS PAPADIAS <andreaspapadias@gmail.com>
2022-07-14 12:41:45 +0300

    buttons ready charts on the making

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 </head>
 <body>
 
-<<<<<<< HEAD
     <div id='map'></div>
     <script>
         mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRndWFuIiwiYSI6ImNpcG50N2s4NDAwNGRmbG5jeXZtMHkxMW4ifQ.ubiXybBxhpidF83H-Zvz7Q';
@@ -26,26 +25,3 @@
     </script>
 </body>
 </html>
-=======
-    <section class="index-intro">
-        <h1>Welcome to Admin's Main Page</h1>
-    </section>
-
-    <?php
-        if(isset($_POST['btn-u'])){
-            echo 'You just clicked Upload button';
-        }
-        if(isset($_POST['btn-e'])){
-            echo 'You just clicked Edit button';
-        }
-        if(isset($_POST['btn-dd'])){
-            echo 'You just clicked Delete Data button';
-        }
-    ?>
-    <form method="post"> 
-        <input type="submit" name="btn-u" value ="Upload"> 
-        <input type="submit" name="btn-e" value ="Edit"> 
-        <input type="submit" name="btn-dd" value ="Delete Data"> 
-    </form>
-      
->>>>>>> eff02e3 (insert/edit/delete buttons)
