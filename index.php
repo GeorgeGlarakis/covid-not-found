4bae122401dfca44c2f9216c8aeff04a60b1d755
ANDREAS PAPADIAS <andreaspapadias@gmail.com>
2022-07-14 12:41:45 +0300

    buttons ready charts on the making

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php>
+<?php
     include_once 'header.php';
-<?>
+?>
 
     <section class="index-intro">
         <h1>Welcome to Admin's Main Page</h1>
---
+++ scripts.php
@@ -0,0 +1,19 @@
+<!-- Chart1 -->
+   <div>
+      <?php
+//   $total_website_views = total_views($conn); // Returns total website views
+  $total_website_views = 5;
+  echo "<strong>Total Website Views:</strong> " . $total_website_views;
+  ?>
+</div>
+
+<!-- Chart2 -->
+   <div>
+         <?php
+   //   $total_proved_cases = total_cases($conn); // Returns total confirmed cases
+   $total_proved_cases = 12;
+   echo "<strong>Total Proved Cases:</strong> " . $total_proved_cases;
+   ?>
+   </div>
+
+<!-- Chart3 -->
