978c636885333625e3e2c2e7a87637a29c105325
Andreas Papadias <andreaspapadias@gmail.com>
2022-07-19 12:16:41 +0300

    latest version of all

---
+++ AdminButtons.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang ="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>index</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+
+        <nav>
+            <div class="wrapper">
+                <!-- <a href="index.php"><img src="img/(logo's file name)" alt="Blogs logo"> -->
+                <button id="insert">Insert Data</button>
+                <input type="file" id="jsonfileinput"/>
+                <button id="edit">Edit Data</button>
+                <button id="delete">Delete Data</button>
+                
+            </div>
+        </nav> 
+            
+    </body>
+</html>
