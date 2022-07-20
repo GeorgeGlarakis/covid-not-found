fcd53422dfb6f05f0dc2705feecc6b1360c9c4f3
Nodas <nodas.pap13@gmail.com>
2022-07-21 00:28:09 +0300

    recent changes

--- leaflet.php
+++ leaflet.php
@@ -66,15 +66,15 @@
         osm.addTo(map);
         map.addLayer(osm)
 
-        // water color 
-        var watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            subdomains: 'abcd',
-            minZoom: 1,
-            maxZoom: 16,
-            ext: 'jpg'
-        });
-        watercolor.addTo(map)
+        // // water color 
+        // var watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+        //     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        //     subdomains: 'abcd',
+        //     minZoom: 1,
+        //     maxZoom: 16,
+        //     ext: 'jpg'
+        // });
+        // watercolor.addTo(map)
 
         // dark map 
         var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
