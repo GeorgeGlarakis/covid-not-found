0ec4162a5d3673fa1918fb0460dc05ab4d9f35d6
Nodas <nodas.pap13@gmail.com>
2022-07-23 13:43:20 +0300

    test page

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leaflet Tutorial</title>
+    <title>Realtime location tracker</title>
 
     <!-- leaflet css  -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
@@ -19,167 +19,58 @@
             width: 100%;
             height: 100vh;
         }
-
-        .coordinate {
-            position: absolute;
-            bottom: 10px;
-            right: 50%;
-        }
-
-        .leaflet-popup-content-wrapper {
-            background-color: #000000;
-            color: #fff;
-            border: 1px solid red;
-            border-radius: 0px;
-        }
     </style>
 </head>
 
 <body>
-    <div id="map">
-        <div class="leaflet-control coordinate"></div>
-    </div>
+    <div id="map"></div>
 </body>
-
 </html>
 
 <!-- leaflet js  -->
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-<script src="./data/point.js"></script>
-<script src="./data/line.js"></script>
-<script src="./data/polygon.js"></script>
-
-
 <script>
     // Map initialization 
-    var map = L.map('map').setView([28.3949, 84.1240], 8);
+    var map = L.map('map').setView([14.0860746, 100.608406], 12);
 
-
-
-    /*==============================================
-                TILE LAYER and WMS
-    ================================================*/
     //osm layer
     var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
     osm.addTo(map);
-    // map.addLayer(osm)
 
-    // water color 
-    var watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        subdomains: 'abcd',
-        minZoom: 1,
-        maxZoom: 16,
-        ext: 'jpg'
-    });
-    // watercolor.addTo(map)
+    if(!navigator.geolocation) {
+        console.log("Your browser doesn't support geolocation feature!")
+    } else {
+        setInterval(() => {
+            navigator.geolocation.getCurrentPosition(getPosition)
+        }, 5000);
+    }
 
-    // dark map 
-    var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 19
-    });
-    // dark.addTo(map)
+    var marker, circle;
 
-    // google street 
-    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
-    // googleStreets.addTo(map);
+    function getPosition(position){
+        // console.log(position)
+        var lat = position.coords.latitude
+        var long = position.coords.longitude
+        var accuracy = position.coords.accuracy
 
-    //google satellite
-    googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
-    // googleSat.addTo(map)
-
-    var wms = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
-        layers: 'geoapp:admin',
-        format: 'image/png',
-        transparent: true,
-        attribution: "wms test"
-    });
-
-
-
-    /*==============================================
-                        MARKER
-    ================================================*/
-    var myIcon = L.icon({
-        iconUrl: 'img/red_marker.png',
-        iconSize: [40, 40],
-    });
-    var singleMarker = L.marker([28.3949, 84.1240], { icon: myIcon, draggable: true });
-    var popup = singleMarker.bindPopup('This is the Nepal. ' + singleMarker.getLatLng()).openPopup()
-    popup.addTo(map);
-
-    var secondMarker = L.marker([29.3949, 83.1240], { icon: myIcon, draggable: true });
-
-    console.log(singleMarker.toGeoJSON())
-
-
-    /*==============================================
-                GEOJSON
-    ================================================*/
-    var pointData = L.geoJSON(pointJson).addTo(map)
-    var lineData = L.geoJSON(lineJson).addTo(map)
-    var polygonData = L.geoJSON(polygonJson, {
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup(`<b>Name: </b>` + feature.properties.name)
-        },
-        style: {
-            fillColor: 'red',
-            fillOpacity: 1,
-            color: '#c0c0c0',
+        if(marker) {
+            map.removeLayer(marker)
         }
-    }).addTo(map);
 
+        if(circle) {
+            map.removeLayer(circle)
+        }
 
+        marker = L.marker([lat, long])
+        circle = L.circle([lat, long], {radius: accuracy})
 
-    /*==============================================
-                    LAYER CONTROL
-    ================================================*/
-    var baseMaps = {
-        "OSM": osm,
-        "Water color map": watercolor,
-        'Dark': dark,
-        'Google Street': googleStreets,
-        "Google Satellite": googleSat,
-    };
-    var overlayMaps = {
-        "First Marker": singleMarker,
-        'Second Marker': secondMarker,
-        'Point Data': pointData,
-        'Line Data': lineData,
-        'Polygon Data': polygonData,
-        'wms': wms
-    };
-    // map.removeLayer(singleMarker)
+        var featureGroup = L.featureGroup([marker, circle]).addTo(map)
 
-    L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+        map.fitBounds(featureGroup.getBounds())
 
-
-    /*==============================================
-                    LEAFLET EVENTS
-    ================================================*/
-    map.on('mouseover', function () {
-        console.log('your mouse is over the map')
-    })
-
-    map.on('mousemove', function (e) {
-        document.getElementsByClassName('coordinate')[0].innerHTML = 'lat: ' + e.latlng.lat + 'lng: ' + e.latlng.lng;
-        console.log('lat: ' + e.latlng.lat, 'lng: ' + e.latlng.lng)
-    })
-
-
-    /*==============================================
-                    STYLE CUSTOMIZATION
-    ================================================*/
-
+        console.log("Your coordinate is: Lat: "+ lat +" Long: "+ long+ " Accuracy: "+ accuracy)
+    }
 
 </script>
