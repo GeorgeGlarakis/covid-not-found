0d216238a48ccdebfda36a582003ae3ee5770282
George Glarakis <george.glarakis@gmail.com>
2022-07-25 20:39:40 +0300

    [DONE] Delete ALL DATA

---
+++ insertdata.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Covid</title> 
+        <!-- <link rel="stylesheet" href="css/style.css"> -->
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"        
+                integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+                crossorigin="anonymous"></script>
+		<script	src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+		
+    </head>
+
+    <body>
+        <!-- Upload File -->
+        <div>
+            Upload a JSON file with POI info: <input type="file" id="jsonfileinput" name="jsonfileinput" />
+
+            <button id="save">Save</button>
+            <button id="delete">Delete ALL DATA</button>
+
+            <script defer type="text/javascript">
+                
+                $(document).ready(function () {
+
+                    $('#save').click(function () {
+                        var file_to_read = document.getElementById("jsonfileinput").files[0];
+                        var fileread = new FileReader();
+                        var myJSON;
+
+                        fileread.readAsText(file_to_read);
+                        fileread.onload = function() {
+                            myJSON = JSON.parse(fileread.result);
+                            console.log(myJSON);                        
+                        }
+
+                        fileread.onloadend = function() {
+                            var sendfile = JSON.stringify(myJSON);
+
+                            $.ajax( {
+                                url: "insert_pois.php",
+                                dataType: "text",
+                                type: "POST",
+                                data: {
+                                    pois: sendfile 
+                                }, 
+                                success: function( data, status, xhr ) {
+                                    console.log(data)
+                                    // if(xhr.status === 301) {
+                                        // alert(xhr.getAllResponseHeaders('location'));
+                                    // }
+                                    
+                                    // window.location.href = response.location;
+                                },
+                                error: function( xhr, status, error ) {
+                                    console.log(error)
+                                }
+                            });
+                        }
+                        fileread.onerror = function() {
+                            console.log(fileread.error);
+                        }
+
+                    });
+
+                    $('#delete').click(function () {
+                        
+                         $.ajax( {
+                            url: "insert_pois.php",
+                            dataType: "text",
+                            type: "POST",
+                            data: { delete: null }, 
+                            success: function( response ) { console.log(response) },
+                            error: function( error ) { console.log(error) }
+                        });
+                       
+
+                    });
+
+                })              
+
+            </script>
+        </div>
+
+    </body>
+</html>
+
