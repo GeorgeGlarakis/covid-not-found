f9c7f87df731a9dbf45a8c4b2ff633c8046fbb50
Nodas <nodas.pap13@gmail.com>
2022-07-25 20:21:41 +0300

    visits

--- visits/visits.inc.php
+++ visits/visits.inc.php
@@ -7,12 +7,16 @@ header('Content-Type: text/plain');
 if (isset($_POST['visits'])) {
     
 	$recived = utf8_encode($_POST['visits']);
-    $confcase = json_decode($recived);
-    $boolean = $boolean->boolean;
-    $user_id = $confcase->user_id;
+    $visits = json_decode($recived);
+    $user_id = $visits->user_id;
+    $poi_id = $visits->poi_id;
+    $estimation = $visits->estimation;
+    date_default_timezone_set("Europe/Athens");
+    $visit_time =  date_create()->format('Y-m-d H:i:s');
 
-    $insert_confcase = "INSERT INTO visits (user_id, boolean)
-                    VALUES ('$user_id', '$boolean');";
+
+    $insert_visits = "INSERT INTO visits (user_id, poi_id, visit_time, estimation)
+                    VALUES ('$user_id', '$poi_id', '$visit_time', '$estimation');";
 
     try {
         $stmt = mysqli_stmt_init($conn);
