fa67351f3f15f899a544cb5c004ad6e91b76d080
George Glarakis <george.glarakis@gmail.com>
2022-08-09 22:21:53 +0300

    Create static_jvascript

---
+++ static_javascript/map_functions.js
@@ -0,0 +1,224 @@
+// Initialize color markers
+var blueMarker = L.AwesomeMarkers.icon({
+    markerColor: 'blue'
+});
+var greenMarker = L.AwesomeMarkers.icon({
+    markerColor: 'green'
+});
+var orangeMarker = L.AwesomeMarkers.icon({
+    markerColor: 'orange'
+});
+var redMarker = L.AwesomeMarkers.icon({
+    markerColor: 'red'
+});
+
+// Map initialization 
+var map = L.map('map').setView([38.29037783868629, 21.79569292607424], 12);
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+osm.addTo(map);
+
+
+window.addEventListener('resize', 
+	() => map.getViewPort().resize());
+
+// Request access to geolocation
+if(!navigator.geolocation) { console.log("Your browser doesn't support geolocation feature!")
+} else { navigator.geolocation.getCurrentPosition(getPosition); }
+
+// Get user's position
+var coords, marker, circle;
+
+function getPosition( position ){
+    var lat = position.coords.latitude
+    var lng = position.coords.longitude
+    var accuracy = position.coords.accuracy
+
+    if(mylocation) { map.removeLayer(mylocation) }
+
+    mycoords = L.marker([lat, lng], {icon: blueMarker}).bindPopup("My Location")
+    mycircle = L.circle([lat, lng], {radius: accuracy})
+
+    var mylocation = L.featureGroup([mycoords, mycircle]).addTo(map)
+
+    map.fitBounds(mylocation.getBounds())
+}
+
+
+// Search for POIs nearby
+
+var pois;
+var user_id ='<?php echo $_SESSION["user_id"];?>';
+
+$(document).ready(function () {
+
+    var name = document.getElementById("searchbyname");
+    var type = document.getElementById("searchbytype");
+
+    name.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            type.value = '';
+            if (pois) { map.removeLayer(pois); }
+            var bounds = map.getBounds(); 
+            getPOIs(bounds, "name", name.value);
+        }
+    });
+
+    type.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            name.value = '';
+            if (pois) { map.removeLayer(pois); }
+            var bounds = map.getBounds(); 
+            getPOIs(bounds, "type", type.value);
+        }
+    });
+})
+
+// Fetch POIs
+function getPOIs( bounds, search_by, value ) {
+    var search = {
+        northEast: {
+            lat: bounds._northEast.lat,
+            lng: bounds._northEast.lng
+        },
+        southWest: {
+            lat: bounds._southWest.lat,
+            lng: bounds._southWest.lng
+        },
+        search_by: search_by,
+        search_value: value
+    }
+
+    $.ajax( {
+        url: "getPOIs.inc.php",
+        dataType: "text",
+        type: "POST",
+        data: {
+            search: JSON.stringify({
+                search
+            })
+        }, 
+        success: function( response ) {
+            if (response.includes("error")) {
+                var error = JSON.parse(response);
+                console.log(error.error);
+            }
+            else {
+                display_pois(JSON.parse(response));
+            }
+        },
+        error: function( xhr, ajaxOptions, thrownError ) {
+            console.log("AJAX Error:" + xhr.status)
+            console.log("Thrown Error:" + thrownError)
+        }
+    });
+}
+
+// Display POIs on the map
+function display_pois( poi_res ) {
+    let markers = [];
+    Object.entries(poi_res).forEach(([key, value]) => {
+        let poi_id = value.poi_id;
+        let name = value.name;
+        let address = value.address;
+        let lat = value.lat;
+        let lng = value.lng;
+        let rating = value.rating;
+        let rating_n = value.rating_n;
+        let populartimes = value.populartimes;
+
+        let pred = crowd_prediction(JSON.parse(populartimes));
+        let marker_color;
+        if (pred.first_hour < 33) { marker_color = greenMarker; }
+        else if (pred.first_hour > 32 && pred.first_hour < 66) { marker_color = orangeMarker; }
+        else { marker_color = redMarker; }
+        let estim = get_estimation(poi_id);            
+
+        let popupText = name + "<br>" + address + "<br>Rating: " + rating + 
+                    "<br>Crowd Prediction: " + pred.first_hour + " " + pred.second_hour +
+                    "<br>Crowd Estimation: " + estim +
+                    '<br>Your crowd estimation: <input type="number" id="estimation_'+ poi_id +'">' +
+                    '<br><input type="button" id="'+ poi_id +'" class="mybuttons" value="Register Visit"></input>';
+        
+        markers[key] = new L.marker([lat, lng], {icon: marker_color})
+                            .bindPopup(popupText)
+                            .on("popupopen", function() {
+                                $('.mybuttons').click(function () {
+                                    var estimation = $('#estimation_' + this.id).val();
+                                    register_visit(user_id, this.id, estimation);
+                                });
+                            });
+    })
+
+    pois = L.layerGroup(markers);
+    map.addLayer(pois);
+}
+
+// Get current crowd prediction
+function crowd_prediction( populartimes ) {
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    var current_day = today.getDay();
+    if (current_day == 0) { current_day = 6; }  // Our JSON starts the week from Monday
+        else { current_day--; }                 // but .getDay() function starts from Sunday
+    const current_time = today.getHours();
+    let weekday, hours;
+    Object.entries(populartimes).forEach(([key, value]) => {
+        if (current_day == key) {
+            weekday = value.name;
+            hours = value.data;                
+        } 
+    })
+
+    return {
+        "first_hour":hours[current_time+1], 
+        "second_hour":hours[current_time+2]
+    };         
+}
+
+// Get User's estimation
+function get_estimation( poi_id ) {
+    $.ajax({
+        url:"getPOIs.inc.php",
+        method:"POST",
+        data:{ estimation: poi_id },
+        dataType:"text",
+        async: false,
+
+        success: function( response ) {
+            if (response.includes("error")) {
+                result = JSON.parse(response).error;
+            }
+            else if (response.includes("estimation")) {
+                result = JSON.parse(response).estimation;
+            }
+        },
+        error: function( xhr, ajaxOptions, thrownError ) {
+            console.log("AJAX Error:" + xhr.status)
+            console.log("Thrown Error:" + thrownError)
+            result = "Error!";
+        }
+    });
+    return result;
+}
+
+// Register visit to the DB
+function register_visit( user_id, poi_id, estimation ) {
+    $.ajax( {
+        url: "../visits/visits.inc.php",
+        dataType: "text",
+        type: "POST",
+        data: {
+            visits: JSON.stringify({ 
+                user_id: user_id,
+                poi_id: poi_id,
+                estimation: estimation
+            })
+        }, 
+        success: function( response ) {
+            alert(response);
+        },
+        error: function( error ) {
+            console.log(error)
+        }
+    });
+}
